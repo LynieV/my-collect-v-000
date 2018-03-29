b918b918867a012251e62d264f0a3ec8f88fbdc7
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-03-29 23:51:16 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -8,15 +8,6 @@ def my_collect(collection)
   collection_new
 end
   
-#if block_given?
-  #i = 0
-  #while i < collection.length
-    #yield(collection[i])
-    #i = i + 1
-  #end
-  #collection
-#else
-  
-#end
+
 
 
